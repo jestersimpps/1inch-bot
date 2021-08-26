f7f78aace29a5d3a84b18ea6939e0e3155e421a5
jestersimpps <jestersimpps@gmail.com>
2021-08-26 20:43:24 +0200

    solve api busy error

--- src/auto-trade.ts
+++ src/auto-trade.ts
@@ -13,54 +13,65 @@ export const limitBuy = async (api: Api, db, tokens, order: TradeParam) => {
   const tradeToken = tokens.find((t) => t.symbol === order.symbol);
   const quoteToken = tokens.find((t) => t.symbol === order.quote);
 
-  const currentPrice = tradeToken.price / quoteToken.price;
-  if (order.amount > quoteToken.balance) {
-    console.log(`BUY ${order.amount} ${tradeToken.symbol} - not enough balance - balance: ${quoteToken.balance}`);
-  } else {
-    if (currentPrice <= order.price) {
-      const swap = await api.swap(quoteToken, tradeToken, order.amount, order.slippage, order.chain);
-      await updateExecuted(db, order);
-      console.log(swap);
+  if (tradeToken && quoteToken) {
+    const currentPrice = tradeToken.price / quoteToken.price;
+
+    if (order.amount > quoteToken.balance) {
+      console.log(`BUY ${order.amount} ${tradeToken.symbol} - not enough balance - balance: ${quoteToken.balance}`);
     } else {
-      console.log(`BUY ${order.amount} ${tradeToken.symbol} when ${tradeToken.symbol}/${quoteToken.symbol} price is below ${order.price} - last price: ${currentPrice}`);
+      if (currentPrice <= order.price) {
+        const swap = await api.swap(quoteToken, tradeToken, order.amount, order.slippage, order.chain);
+        await updateExecuted(db, order);
+        console.log(swap);
+      } else {
+        console.log(`BUY ${order.amount} ${tradeToken.symbol} when ${tradeToken.symbol}/${quoteToken.symbol} price is below ${order.price} - last price: ${currentPrice}`);
+      }
     }
+  } else {
+    console.log(`API busy`);
   }
 };
 
 export const limitSell = async (api: Api, db, tokens, order: TradeParam) => {
   const tradeToken = tokens.find((t) => t.symbol === order.symbol);
   const quoteToken = tokens.find((t) => t.symbol === order.quote);
+  if (tradeToken && quoteToken) {
+    const currentPrice = tradeToken.price / quoteToken.price;
 
-  const currentPrice = tradeToken.price / quoteToken.price;
-
-  if (order.amount > tradeToken.balance) {
-    console.log(`SELL ${order.amount} ${tradeToken.symbol} - not enough balance - balance: ${tradeToken.balance}`);
-  } else {
-    if (currentPrice >= order.price) {
-      const swap = await api.swap(tradeToken, quoteToken, order.amount, order.slippage, order.chain);
-      await updateExecuted(db, order);
-      console.log(swap);
+    if (order.amount > tradeToken.balance) {
+      console.log(`SELL ${order.amount} ${tradeToken.symbol} - not enough balance - balance: ${tradeToken.balance}`);
     } else {
-      console.log(`SELL ${order.amount} ${tradeToken.symbol} when ${tradeToken.symbol}/${quoteToken.symbol} price is above ${order.price} - last price: ${currentPrice}`);
+      if (currentPrice >= order.price) {
+        const swap = await api.swap(tradeToken, quoteToken, order.amount, order.slippage, order.chain);
+        await updateExecuted(db, order);
+        console.log(swap);
+      } else {
+        console.log(`SELL ${order.amount} ${tradeToken.symbol} when ${tradeToken.symbol}/${quoteToken.symbol} price is above ${order.price} - last price: ${currentPrice}`);
+      }
     }
+  } else {
+    console.log(`API busy`);
   }
 };
 
 export const stopLoss = async (api: Api, db, tokens, order: TradeParam) => {
   const tradeToken = tokens.find((t) => t.symbol === order.symbol);
   const quoteToken = tokens.find((t) => t.symbol === order.quote);
+  if (tradeToken && quoteToken) {
+    const currentPrice = tradeToken.price / quoteToken.price;
 
-  const currentPrice = tradeToken.price / quoteToken.price;
-
-  if (order.amount > tradeToken.balance) {
-    console.log(`SELL ${order.amount} ${tradeToken.symbol} - not enough balance - balance: ${tradeToken.balance}`);
-  } else {
-    if (currentPrice <= order.price) {
-      const swap = await api.swap(tradeToken, quoteToken, order.amount, order.slippage, order.chain);
-      await updateExecuted(db, order);
-      console.log(swap);
+    if (order.amount > tradeToken.balance) {
+      console.log(`SELL ${order.amount} ${tradeToken.symbol} - not enough balance - balance: ${tradeToken.balance}`);
     } else {
-      console.log(`SELL ${order.amount} ${tradeToken.symbol} when ${tradeToken.symbol}/${quoteToken.symbol} price is below ${order.price} - last price: ${currentPrice}`);
+      if (currentPrice <= order.price) {
+        const swap = await api.swap(tradeToken, quoteToken, order.amount, order.slippage, order.chain);
+        await updateExecuted(db, order);
+        console.log(swap);
+      } else {
+        console.log(`SELL ${order.amount} ${tradeToken.symbol} when ${tradeToken.symbol}/${quoteToken.symbol} price is below ${order.price} - last price: ${currentPrice}`);
+      }
     }
+  } else {
+    console.log(`API busy`);
   }
 };
